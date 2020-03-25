691277c3f133152428dc9e76c01f6c71c4c7e58a
root <root@GYSLAP308.localdomain>
2020-03-25 17:29:03 -0500

    primer commit

---
+++ infra.tf
@@ -0,0 +1,13 @@
+variables
+
+resource "azurerm_storage_account" "example" {
+  name                     = "stac"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
