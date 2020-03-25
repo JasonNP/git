bd9d4439cbba2cb959f3e25ee0e9ec7fc030abdc
root <root@GYSLAP308.localdomain>
2020-03-25 17:55:13 -0500

    partes

---
+++ parte1/infra_p1.tf
@@ -0,0 +1,15 @@
+variable numero {
+  default=01
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "stac_p1_"${var.numero}
+  resource_group_name      = RSGREU2XXXXD01
+  location                 = eastus2
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "develop"
+  }
+}
--- parte2/infra_p2.tf
+++ parte2/infra_p2.tf
@@ -1,9 +1,9 @@
 variable numero {
-  default=1
+  default=01
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "stac"${var.numero}
+  name                     = "stac_p2_"${var.numero}
   resource_group_name      = RSGREU2XXXXD01
   location                 = eastus2
   account_tier             = "Standard"
